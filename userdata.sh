ad693d4d0678d3e7e664306e58f6e2d351d2ecc1
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-08-29 11:41:12 +0530

    vpc ceration

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,4 @@
 
-
+set-hostname -skip-apply
 labauto ansible
 ansible-pull -i localhost -U https://github.com/Rajesh-2406/roboshop-ansible main.yml -e role_name=rabbitmq -e env=${env} &>>/opt/ansible.log
