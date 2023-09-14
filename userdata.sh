c55b416bbedb1d88a4fe867b695e31a99e57dfc2
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-09-14 14:45:32 +0530

    terraform

--- userdata.sh
+++ userdata.sh
@@ -2,4 +2,4 @@
 
 set-hostname -skip-apply ${component}
 labauto ansible
-ansible-pull -i localhost, -U https://github.com/Rajesh-2406/roboshop-ansible main.yml -e role_name=rabbitmq -e env=${env} &>>/opt/ansible.log
+ansible-pull -i localhost, -U https://github.com/Rajesh-2406/roboshop-ansible.git main.yml -e role_name=rabbitmq -e env=${env} &>>/opt/ansible.log
