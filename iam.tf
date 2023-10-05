59c9c7f34f5dac54bb747b13efb9c2045d88be63
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-10-05 13:20:51 +0530

    nexus

--- iam.tf
+++ iam.tf
@@ -47,3 +47,8 @@ resource "aws_iam_instance_profile" "instance_profile"  {
   name  = "${var.component}-${var.env}-ec2-role"
   role  = aws_iam_role.role.name
 }
+
+resource "aws_iam_role_policy_attachment" "policy-attach" {
+  policy_arn = aws_iam_role.role.arn
+  role       = aws_iam_role.role.name
+}
