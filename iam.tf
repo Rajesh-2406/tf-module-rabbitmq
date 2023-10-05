aa34b7f18562f342c973b5098bffb5485e0dbf5d
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-10-05 21:11:23 +0530

    nexus

--- iam.tf
+++ iam.tf
@@ -17,7 +17,7 @@ resource "aws_iam_policy" "policy" {
           "kms:Decrypt"
         ],
         "Resource" : [
-          "arn:aws:ssm:us-east-1:600222537277:parameter/roboshop.${var.env}.${var.component}.*",
+          "arn:aws:kms:us-east-1:765371225572:parameter/roboshop.${var.env}.${var.component}.*",
            var.kms_key_arn
           ]
       }
